8f044adc6876e40d53d547608c188134873881ca
larkin-nz <daniel@larkin.nz>
2021-07-09 16:29:16 +1200

    Finished equals function

--- tests/matrix.js
+++ tests/matrix.js
@@ -331,8 +331,6 @@ test('equals (out of epsilon)', t => {
     [1, 2.0005]
   ]);
 
-  console.log(a, b)
-
   // Check the matrix has computed correctly
   if (a.equals(b, true) == false) t.pass();
 });
